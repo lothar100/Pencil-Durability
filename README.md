e1a4b7e95dab984e6d6cb07b3ef59d31fb23e566
Pietr Langevoort <infrared.skittle@gmail.com>
2019-02-05 12:01:02 -0500

    Last Commit
    Added information to the README
    Added argument handling for commands
    Added launch setting that allows typed arguments
    Added comments to code

--- README.md
+++ README.md
@@ -1,8 +1,35 @@
-## Pencil Durability
+## Pencil Durability - Commands
 
-## Print
+Format: (Terminal >) [Command] [Params]
+
+Test - no params - will run tests from PencilTest.java
+Write - param1: String - will write the param string onto the paper
+Sharpen - no params - will attempt to sharpen the point, increasing point durability
+Erase - param1: String - will erase last occurance of the param string from the paper
+Edit - param1: String - will attempt to insert param string into the location of the last erased text
+Print - no params - will display in the console text currently on the page
+ClearPage - no params - will remove all text and erase data from the paper
+
+Completion: "[Command] complete" should display in the console
+
+# Setup
+
+For this programming kata I used Java and Visual Studio Code as my IDE.
+If you do the same you will want to install the extension "Java Extension Pack". It has everything you will need to run Java.
+Dependancies should be already setup to use a hard reference.
+pencil/.vscode/lanuch.json contains two debug launch configurations for VS Code. One is reciving arguments upon launch, the other is hard set with the argument "Test".
+workspace1.code-workspace is a VS Code workspace file. Needs to be present with VS Code or extensions will throw workspace related errors.
+
+# Print
 *As a writer  
 I want to be able to see what is written on the my paper  
 so that I can better review my thoughts*  
 
 When the pencil is instructed to print the currently held text will be display in the console.
+
+# ClearPage
+*As a writer  
+I want to be able to write on a new piece of paper  
+so that I can better organize my thoughts*  
+
+When the pencil is instructed to clear page text and erase data will both be reset.
--- pencil/src/main/Pencil.java
+++ pencil/src/main/Pencil.java
@@ -13,10 +13,11 @@ public class Pencil {
     public int bodyLength;
     public int eraserDurability;
     
-    private int sharpeningRate = 40000;
+    private int sharpeningRate = 40000; // for future use, possibly different sharpening rates for different sharpeners
 
     private EraseData eData = null;
 
+    //default
     public Pencil() {
         Text = "";
         pointDurability = 40000;
@@ -25,6 +26,10 @@ public class Pencil {
         eraserDurability = 2000;
     }
 
+    // Params: iText = text on a sheet of paper,
+    //          iPoint = initial point durability
+    //          iBodyLength = initial body length
+    //          iEraser = initial eraser durability
     public Pencil(String iText, int iPoint, int iBodyLength, int iEraser) {
         Text = iText;
         pointDurability = iPoint;
@@ -34,6 +39,8 @@ public class Pencil {
     }
 
     public static void main(String[] args) throws Exception {
+        Pencil temp = new Pencil();
+
         if(args[0].equals("Test")){
             Result result = JUnitCore.runClasses(PencilTest.class);
 
@@ -55,18 +62,61 @@ public class Pencil {
                     System.out.print("\n" + numIgnored + " Ignored");
                 }
             }
+            System.out.println("\nTest complete");
+        }
+        if(args[0].equals("Write")){
+            if(args[1]!=null && !args[1].isEmpty()){
+                temp.Write(args[1]);
+            }
+            System.out.println("\nWrite complete");
+        }
+        if(args[0].equals("Sharpen")){
+            temp.Sharpen();
+            System.out.println("\nSharpen complete");
+        }
+        if(args[0].equals("Erase")){
+            if(args[1]!=null && !args[1].isEmpty()){
+                temp.Erase(args[1]);
+            }
+            System.out.println("\nErase complete");
+        }
+        if(args[0].equals("Edit")){
+            if(args[1]!=null && !args[1].isEmpty()){
+                temp.Edit(args[1]);
+            }
+            System.out.println("\nEdit complete");
+        }
+        if(args[0].equals("Print")){
+            temp.Print();
+            System.out.println("\nPrint complete");
+        }
+        if(args[0].equals("ClearPage")){
+            temp.ClearPage();
+            System.out.println("\nClearPage complete");
         }
     }
 
+    //[Command - Write]
+    //Story: As a writer
+    //      I want to be able use a pencil to write text on a sheet of paper
+    //      so that I can better remember my thoughts
     public void Write(String newText) {
         Text+=newText;
         degradePoint(countDurabilityUse(newText));
     }
 
+    //[Command - Sharpen]
+    //Story: As a writer
+    //      I want to be able to sharpen my pencil
+    //      so that I can continue to write with it after it goes dull
     public void Sharpen() {
         sharpenPoint(sharpeningRate);
     }
 
+    //[Command - Erase]
+    //Story: As a writer
+    //      I want to be able to erase previously written text
+    //      so that I can remove my mistakes
     public void Erase(String eraseText) {
         if(eraserDurability<1) return;
         if(!Text.contains(eraseText)) return;
@@ -85,6 +135,10 @@ public class Pencil {
         Text = new String(characters);
     }
 
+    //[Command - Edit]
+    //Story: As a writer
+    //      I want to be able to edit previously written text
+    //      so that I can change my writing without starting over
     public void Edit(String newText) {
         if(eData==null) return;
 
@@ -115,19 +169,37 @@ public class Pencil {
         eData=null;
     }
 
+    //[Extra Command - Print]
+    //Story: As a writer
+    //      I want to be able to see what is written on the my paper
+    //      so that I can better review my thoughts
     public void Print() {
         System.out.println(Text);
     }
 
+    //[Extra Command - ClearPage]
+    //Story: As a writer
+    //      I want to be able to write on a new piece of paper 
+    //      so that I can better organize my thoughts
+    //
+    //  Use to clear text and remove erase data at the same time
     public void ClearPage() {
         Text="";
         eData=null;
     }
 
+    //[Required - Eraser Degradation]
+    //Story: As a pencil manufacturer
+    //      I want a pencil eraser to eventually wear out
+    //      so that I can sell more pencils
     private void degradeEraser() {
         eraserDurability--;
     }
 
+    //[Required - Point Degradation]
+    //Story: As a pencil manufacturer
+    //      I want writing to cause a pencil point to go dull
+    //      so that I can sell more pencils
     private void degradePoint(int lossDur) {
         pointDurability = Math.max(0,pointDurability-lossDur);
     }
@@ -139,6 +211,8 @@ public class Pencil {
         bodyLength--;
     }
 
+    //Use to count the expected point durability use for a given input string
+    //Rules: Uppercase characters = 2 durability, All other chars = 1 durability
     public int countDurabilityUse(String input) {
         int result = 0;
         char[] characters = input.replaceAll("\\s+","").toCharArray();
